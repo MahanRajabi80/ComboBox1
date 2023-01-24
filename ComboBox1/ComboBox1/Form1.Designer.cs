193dc8301d9059b0bda3a2116f71006bda563834
MahanRajabi80 <105061018+MahanRajabi80@users.noreply.github.com>
2023-01-24 16:35:31 +0330

    Add files via upload

---
+++ ComboBox1/ComboBox1/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ComboBox1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmBoxOstan = new System.Windows.Forms.ComboBox();
+            this.cmBoxCity = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // cmBoxOstan
+            // 
+            this.cmBoxOstan.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmBoxOstan.FormattingEnabled = true;
+            this.cmBoxOstan.Location = new System.Drawing.Point(224, 146);
+            this.cmBoxOstan.Name = "cmBoxOstan";
+            this.cmBoxOstan.Size = new System.Drawing.Size(121, 21);
+            this.cmBoxOstan.TabIndex = 0;
+            this.cmBoxOstan.SelectedIndexChanged += new System.EventHandler(this.cmBoxOstan_SelectedIndexChanged);
+            // 
+            // cmBoxCity
+            // 
+            this.cmBoxCity.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmBoxCity.FormattingEnabled = true;
+            this.cmBoxCity.Location = new System.Drawing.Point(367, 146);
+            this.cmBoxCity.Name = "cmBoxCity";
+            this.cmBoxCity.Size = new System.Drawing.Size(121, 21);
+            this.cmBoxCity.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmBoxCity);
+            this.Controls.Add(this.cmBoxOstan);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmBoxOstan;
+        private System.Windows.Forms.ComboBox cmBoxCity;
+    }
+}
+
---
+++ ComboBox1/ComboBox1/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComboBox1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            object[] ostan = { "ilam", "Tehran", "Mazandaran", "Gilan", "Golestan" };
+            cmBoxOstan.Items.AddRange(ostan);
+        }
+
+        private void cmBoxOstan_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cmBoxCity.Items.Clear();
+            switch (cmBoxOstan.SelectedItem.ToString())
+            {
+                case "ilam":
+                    cmBoxCity.Items.Add("ilam");
+                    break;
+                case "Tehran":
+                    cmBoxCity.Items.Add("Tehran");
+                    cmBoxCity.Items.Add("Rodehen");
+                    cmBoxCity.Items.Add("Damavand");
+                    break;
+                case "Mazandaran":
+                    cmBoxCity.Items.Add("Babol");
+                    cmBoxCity.Items.Add("Sari");
+                    break;
+                case "Gilan":
+                    cmBoxCity.Items.Add("Rasht");
+                    cmBoxCity.Items.Add("Masole");
+                    cmBoxCity.Items.Add("Anzali");
+                    break;
+                case "Golestan":
+                    cmBoxCity.Items.Add("Gorgan");
+                    cmBoxCity.Items.Add("Bandar Torkman");
+                    break;
+            }
+            cmBoxCity.SelectedIndex = 0;
+        }
+    }
+}
